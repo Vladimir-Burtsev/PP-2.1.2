174cdfb746a5bb7789a74d4eaaa80de45be7c160
vladimir burtsev <amakusl2@gmail.com>
2022-10-18 19:47:48 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean.equals(bean2));
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getId());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getId());
+        System.out.println(cat.equals(cat1));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    private Long id;
+    public void setId(Long id) { this.id = id; }
+    public Long getId() { return id; }
+}
